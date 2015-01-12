661d5641427ab1a589401d7a1d6ff32688033c22
nknshm <hihinuma@gmail.com>
2015-01-12 13:34:17 +0900

    api

--- ajaxDriver.js
+++ ajaxDriver.js
@@ -2,10 +2,14 @@
 /*global jQuery */
 module.exports = (function () {
     'use strict';
+    var genBooklogFolderCtrl = require('../booklog/apiInterface'),
+        genBooklogFolder = genBooklogFolderCtrl();
+
     function driver(reqType, param) {
         //console.log('ajax driver');
         //console.log(reqType);
         //console.log(param);
+        genBooklogFolder.genFolder(param.count);
         if (reqType === 'genBKL') {
             return {
                 status: 'OK'
