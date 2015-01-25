236622a9ce166a9ecc283dafce8aef5d8178f734
nknshm <hihinuma@gmail.com>
2015-01-25 17:02:06 +0900

    同期版

--- ajaxDriver.js
+++ ajaxDriver.js
@@ -2,21 +2,39 @@
 /*global jQuery */
 module.exports = (function () {
     'use strict';
+    var
+        genBooklogFolderCtrl = require('../booklog/apiInterface'),
+        genBooklogFolder = genBooklogFolderCtrl();
+
     function driver(reqType, param) {
         //console.log('ajax driver');
         //console.log(reqType);
         //console.log(param);
         if (reqType === 'genBKL') {
+            genBooklogFolder.genFolder(param.count);
             return {
                 status: 'OK'
             };
-        } else {
-            throw new Error('unkown reqType:' + reqType);
         }
+        throw new Error('unkown reqType:' + reqType);
     }
-
+    function driverAsync(reqType, param) {
+        var ret;
+        if (reqType === 'genBKL') {
+            ret = genBooklogFolder.genFolderAsync(param.count)
+                .then(function () {
+                    return {
+                        status: 'OK'
+                    };
+                })
+                .promise();
+            return ret;
+        }
+        throw new Error('unkown reqType:' + reqType);
+    }
     return {
-        driver: driver
+        driver: driver,
+        driverAsync: driverAsync,
     };
 }());
 
