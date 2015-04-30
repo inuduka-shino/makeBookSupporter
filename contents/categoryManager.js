05fb2a4c8cc6e5b9ced8b82f320ce02c0f683126
nknshm <hihinuma@gmail.com>
2015-04-30 13:25:30 +0900

    category pattern

--- contents/categoryManager.js
+++ contents/categoryManager.js
@@ -3,6 +3,7 @@
 md('categoryManager', function () {
     'use strict';
     var categoryDict,
+        jpgFilePattern,
         getCategory,
         genCategoryManager;
 
@@ -12,16 +13,72 @@ md('categoryManager', function () {
             name: 'Unknown',
             extClass: 'list-group-item-warning'
         },
+        'front':  {
+            priority: 200,
+            name: '前付'
+        },
+        'frontispiece':  {
+            priority: 300,
+            name: '口絵'
+        },
+        'eyecatch':  {
+            priority: 400,
+            name: 'アイキャッチ'
+        },
+        'colorbody':  {
+            priority: 530,
+            name: 'カラー'
+        },
+        'body':  {
+            priority: 550,
+            name: '本文'
+        },
+        'back':  {
+            priority: 600,
+            name: '後付'
+        },
+        'supplement':  {
+            priority: 700,
+            name: '付録'
+        },
         'info': {
             priority: 900,
             name: 'information file(xinfo.txt)'
         }
     };
-
+    jpgFilePattern = /^P([A-Z])([A-Za-z]?)([0-9]{3}|)(\w*)\.jpg$/;
     function categoryRule(name) {
+        var parse,
+            pageType;
         if (name === 'xinfo.txt') {
             return 'info';
         }
+        parse = jpgFilePattern.exec(name);
+        if (parse !== null) {
+            pageType = parse[1];
+            if (pageType >= 'A' && pageType <= 'J') {
+                return 'front';
+            }
+            if (pageType === 'K') {
+                return 'frontispiece';
+            }
+            if (pageType === 'L') {
+                return 'eyecatch';
+            }
+            if (pageType === 'M') {
+                return 'colorbody';
+            }
+            if (pageType === 'N') {
+                return 'body';
+            }
+            if (pageType >= 'O' && pageType <= 'Q') {
+                return 'back';
+            }
+            if (pageType >= 'R' && pageType <= 'Z') {
+                return 'supplement';
+            }
+        }
+
 
         return 'unknown';
     }
